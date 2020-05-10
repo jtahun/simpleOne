7bd101bb6ea03cf4c2a1163539952d995b6b5c6c
igor <jtahun@bk.ru>
2020-05-10 18:19:31 +0600

    first on the first.

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+  public static void main (String[] args){
+    System.out.println("Hello Idea!");
+    for (int i = 0; i < 10; i++) {
+      System.out.println("Hello Jack number "+i);
+    }
+  }
+}
